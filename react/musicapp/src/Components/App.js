dc6af4d217e9ac26d100608500b65f5c00c0914b
Sergey Plaski <splaski@gmail.com>
2018-07-26 15:43:31 +0300

    init commit

---
+++ react/musicapp/src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <div className='wrapper'>
+                <div className="container">
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default App;
